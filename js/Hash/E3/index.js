13af48a318413ecc371f6b6aec49f9fe4d0eb4cf
martincorona007 <martincorona007@hotmail.com>
2022-12-21 09:05:08 -0600

    adding rest of hashtable

--- js/Hash/E3/index.js
+++ js/Hash/E3/index.js
@@ -34,4 +34,43 @@ class HashTable{
       }
     }
   }
+  //removes key-value pair from the table
+  remove(key){
+    const index = this.hash(key);
+    const bucket = this.table[index];
+    if(bucket){
+      const sameKeyItem = bucket.find((item)=> item[0] === key);
+      if(sameKeyItem){
+        bucket.splice(bucket.indexOf(sameKeyItem),1);
+      }
+    }
+  }
+  //retrieves a value from the table given the key
+  get(key){
+    const index = this.hash(key);
+    const bucket = this.table[index];
+    if(bucket){
+      const sameKeyItem = bucket.find((item)=> item[0] === key);
+      if(sameKeyItem){
+        bucket.splice(bucket.indexOf(sameKeyItem),1);
+      }
+    }
+  }
+  //prints all the values in table
+  display(){
+    for(let i = 0; i < this.table.length; i++){
+      if(this.table[i]){
+        console.log(i,this.table[i]);
+      }
+    }
+  }
 }
+const table = new HashTable(50);
+table.set("name","Sameen");
+table.set("age",101);
+table.display();
+console.log(table.get("name"));
+table.remove("name");
+table.display();
+console.log("HashTable is empty ? ",table.isEmpty());
+console.log("HashTable size = ",table.getSize());
